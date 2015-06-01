7168f8e336faa3514b756c9d38a9978020027ae2
MoA <honomoa@gmail.com>
2015-06-01 23:30:11 +0800

    add jshint

--- MoaLog.js
+++ MoaLog.js
@@ -1,8 +1,11 @@
-var colors = require('colors').enabled = true,
-        fs = require('fs'),
+'use strict';
+
+var     fs = require('fs'),
       util = require('util'),
       path = require('path'),
     appDir = path.dirname(require.main.filename);
+
+require('colors').enabled = true;
 
 module.exports = MoaLog;
 
@@ -23,49 +26,49 @@ MoaLog.L_ERROR = 4;
 MoaLog.L_CRIT = 5;
 MoaLog.prototype.setLevel = function(level){
   this.level = level;
-}
+};
 MoaLog.prototype.all = function(){
   if(this.level <= MoaLog.LEVEL_ALL){
     console.log.apply(this, Array.prototype.concat('  all'.grey, Array.prototype.slice.call(arguments, 0)));
   }
-}
+};
 MoaLog.prototype.debug = function(){
   if(this.level <= MoaLog.LEVEL_DEBUG){
     console.log.apply(this, Array.prototype.concat('debug'.grey, Array.prototype.slice.call(arguments, 0)));
   }
-}
+};
 MoaLog.prototype.info = function(){
   if(this.level <= MoaLog.LEVEL_INFO){
     console.log.apply(this, Array.prototype.concat(' info'.cyan, Array.prototype.slice.call(arguments, 0)));
   }
-}
+};
 MoaLog.prototype.warn = function(){
   if(this.level <= MoaLog.LEVEL_WARN){
     console.log.apply(this, Array.prototype.concat(' warn'.yellow, Array.prototype.slice.call(arguments, 0)));
   }
-}
+};
 MoaLog.prototype.error = function(){
   if(this.level <= MoaLog.LEVEL_ERROR){
     console.log.apply(this, Array.prototype.concat('error'.magenta, Array.prototype.slice.call(arguments, 0)));
   }
-}
+};
 MoaLog.prototype.crit = function(){
   if(this.level <= MoaLog.LEVEL_CRIT){
     console.log.apply(this, Array.prototype.concat(' crit'.red, Array.prototype.slice.call(arguments, 0)));
   }
-}
-MoaLog.prototype.console = function(type){
+};
+MoaLog.prototype.console = function(){
 //  arguments.length;
   console.log.apply(this, Array.prototype.slice.call(arguments, 0));
-}
+};
 MoaLog.prototype.file = function(name, msg){
   if(msg.toString()==='[object Object]'){
     msg = JSON.stringify(msg);
   }
   fs.writeFileSync(appDir+'/'+name, msg+"\n", {flag: 'a+'});
-}
+};
 MoaLog.prototype.inspect = function(object){
-  console.log(util.inspect(object, { showHidden: true, depth: null }))
-}
+  console.log(util.inspect(object, { showHidden: true, depth: null }));
+};
 
 
